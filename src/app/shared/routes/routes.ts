0a0b2b95d49e7e4465e3c4e0ea6a85869b8dca3b
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-09-07 18:03:18 -0300

    feat: adicionando modulo home

--- src/app/shared/routes/routes.ts
+++ src/app/shared/routes/routes.ts
@@ -3,7 +3,6 @@ import { Routes } from "@angular/router";
 export const content: Routes = [
   {
     path: "home",
-    ///simple-page/first-page
     loadChildren: () => import("../../components/home/home.module").then((m) => m.HomeModule),
   }
 ];
