679afe9f8645f1c2c1013571a5b598811fab218a
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-10-04 21:33:22 -0300

    feat: adicionando tela de plano de ação

--- src/app/components/home/plano-acao/plano-acao.component.ts
+++ src/app/components/home/plano-acao/plano-acao.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './plano-acao.component.html',
   styleUrls: ['./plano-acao.component.scss']
 })
-export class PlanoAcaoComponent {}
+export class PlanoAcaoComponent {
+  currentView: string = 'table';  // Define a visualização inicial como 'table'
+
+  // Dados dos Planos de Ação
+  totalPlanosAcao: number = 16;
+  concluidos: number = 12;
+  emAndamento: number = 0;
+  atrasados: number = 4;
+
+  // Alterna entre as visualizações de Tabela e Gráficos
+  switchView(view: string): void {
+    this.currentView = view;
+  }
+}
